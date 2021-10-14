9d7d0d10917d7a4b469ef6ad89a8619a9f8bacfc
Spike <89262628+lookatspike@users.noreply.github.com>
2021-10-14 18:23:06 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JustScan</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <script>
+        //alert("Cliccando ok dichiari di avere almeno 13 anni")
+    </script>
+    <div id="header">
+        <i style="margin-top: 20px;" >Just Scan</i>
+        
+
+    </div>
+    
+</body>
+</html>
